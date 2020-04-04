2273462a35c34c3505f32e5163051645bbbb21b1
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-04-04 13:27:07 +0200

    Simplify ring finder

--- ctapipe/image/muon/muon_ring_finder.py
+++ ctapipe/image/muon/muon_ring_finder.py
@@ -1,5 +1,4 @@
 import numpy as np
-import astropy.units as u
 from ctapipe.core import Component
 from ctapipe.io.containers import MuonRingParameter
 from .fitting import kundu_chaudhuri_circle_fit, taubin_circle_fit
@@ -9,10 +8,11 @@ import traitlets as traits
 # force the same interface onto them, here.
 # we also modify their names slightly, since the names are
 # exposed to the user via the string traitlet `fit_method`
+
+
 def kundu_chaudhuri(x, y, weights, mask):
     """kundu_chaudhuri_circle_fit with x, y, weights, mask interface"""
-    weights = weights * mask
-    return kundu_chaudhuri_circle_fit(x, y, weights)
+    return kundu_chaudhuri_circle_fit(x[mask], y[mask], weights[mask])
 
 
 def taubin(x, y, weights, mask):
@@ -26,8 +26,7 @@ __all__ = ["MuonRingFitter"]
 
 
 class MuonRingFitter(Component):
-    """Different ring fit algorithms for muon rings
-    """
+    """Different ring fit algorithms for muon rings"""
 
     fit_method = traits.CaselessStrEnum(
         list(FIT_METHOD_BY_NAME.keys()),
@@ -41,12 +40,10 @@ class MuonRingFitter(Component):
         fit_function = FIT_METHOD_BY_NAME[self.fit_method]
         radius, center_x, center_y = fit_function(x, y, img, mask)
 
-        output = MuonRingParameter()
-        output.ring_center_x = center_x
-        output.ring_center_y = center_y
-        output.ring_radius = radius
-        output.ring_center_phi = np.arctan2(center_y, center_x)
-        output.ring_center_distance = np.sqrt(center_x ** 2.0 + center_y ** 2.0)
-        output.ring_fit_method = self.fit_method
-
-        return output
+        return MuonRingParameter(
+            center_x=center_x,
+            center_y=center_y,
+            radius=radius,
+            center_phi=np.arctan2(center_y, center_x),
+            center_distance=np.sqrt(center_x**2 + center_y**2),
+        )
