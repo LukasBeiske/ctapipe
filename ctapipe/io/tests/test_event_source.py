0330f2152cea456a77e2943b7a924245b3797db2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-04-04 11:41:28 +0200

    Add test for event source behaviour

--- ctapipe/io/tests/test_event_source.py
+++ ctapipe/io/tests/test_event_source.py
@@ -36,3 +36,26 @@ def test_is_iterable():
     test_reader = DummyReader(input_url=dataset)
     for _ in test_reader:
         pass
+
+
+def test_from_url_config():
+    from ctapipe.core import Component
+    from traitlets.config import Config
+
+    path = get_dataset_path('gamma_test_large.simtel.gz')
+
+    config = Config()
+    config.input_url = path
+
+    e = EventSource.from_url(config=config)
+    assert e.input_url == path
+
+    class TestComponent(Component):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.eventsource = EventSource.from_url(parent=self)
+
+    parent_config = Config()
+    parent_config.EventSource.input_url = path
+    t = TestComponent(config=parent_config)
+    assert t.eventsource.input_url == path
